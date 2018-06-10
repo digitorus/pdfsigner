17f2bb53de89c7e94bfddf6e71e23f02eb8356b0
tigp <24852530+tigp@users.noreply.github.com>
2018-06-10 19:32:17 +0200

    Utils - add proper comments

--- utils/utils.go
+++ utils/utils.go
@@ -1,24 +1,26 @@
 package utils
 
 import (
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+// GetRunFileFolder returns path to executable file
 func GetRunFileFolder() (string, error) {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	return dir, nil
 }
 
+// IsTestEnvironment checks if it runs inside the test
 func IsTestEnvironment() bool {
 	return strings.HasSuffix(os.Args[0], ".test")
 }
 
+// GetGoPath returns GOPATH environment variable
 func GetGoPath() string {
 	return os.Getenv("GOPATH")
 }
